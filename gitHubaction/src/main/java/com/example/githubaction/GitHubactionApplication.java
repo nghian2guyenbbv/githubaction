5cb5f70f445c5b9655d9adb2d3aabe20c9204d1b
nghianguyenbbv <nghia2nguyen@bbv.vn>
2023-10-20 11:33:45 +0700

    test app

---
+++ gitHubaction/src/main/java/com/example/githubaction/GitHubactionApplication.java
@@ -0,0 +1,12 @@
+package com.example.githubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubactionApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(GitHubactionApplication.class, args);
+  }
+
+}
